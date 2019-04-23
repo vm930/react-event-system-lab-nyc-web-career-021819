2d1cb33674def25cc17d3baa96741e9fce7848cf
Victoria Mei <vm930@hotmail.com>
2019-04-22 23:16:28 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,22 @@
 // Code EyesOnMe Component Here
+import React, { Component } from 'react';
+
+class EyesOnMe extends Component {
+	handleBlur() {
+		console.log('Hey! Eyes on me!');
+	}
+
+	handleFocus() {
+		console.log('Good!');
+	}
+
+	render() {
+		return (
+			<button onFocus={this.handleFocus} onBlur={this.handleBlur}>
+				hello
+			</button>
+		);
+	}
+}
+
+export default EyesOnMe;
